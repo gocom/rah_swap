3383df41e721b2f1754efe63ac25e0ef3b34443e
Jukka Svahn <web@rahinaa.biz>
2013-05-06 11:24:35 +0300

    Updated programming styling.

--- rah_swap.php
+++ rah_swap.php
@@ -12,74 +12,79 @@
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-	function rah_swap($atts, $thing=NULL) {
-		
+	function rah_swap($atts, $thing = null)
+	{	
 		global $rah_swap, $txpcfg, $prefs, $is_article_body;
-		
-		static $default_cfg = NULL;
-		
-		if($default_cfg === NULL) {
+		static $default_cfg = null;
+
+		if ($default_cfg === null)
+		{
 			$default_cfg = $txpcfg;
 		}
-		
-		if($is_article_body) {
-			if(!$prefs['allow_article_php_scripting']) {
+
+		if ($is_article_body)
+		{
+			if (!$prefs['allow_article_php_scripting'])
+			{
 				trigger_error(gTxt('php_code_disabled_article'));
 				return;
 			}
-			
-			elseif(!has_privs('article.php', $thisarticle['authorid'])) {
+			else if(!has_privs('article.php', $thisarticle['authorid']))
+			{
 				trigger_error(gTxt('php_code_forbidden_user'));
 				return;
 			}
 		}
-		
-		elseif(!$prefs['allow_page_php_scripting']) {
+		else if (!$prefs['allow_page_php_scripting'])
+		{
 			trigger_error(gTxt('php_code_disabled_page'));
 			return;
 		}
-		
-		if(isset($atts['link'])) {
-			
-			if(!isset($rah_swap[$atts['link']])) {
+
+		if (isset($atts['link']))
+		{
+			if (!isset($rah_swap[$atts['link']]))
+			{
 				trigger_error(gTxt('invalid_attribute_value', array('{name}' => $atts['link'])));
 				return;
 			}
-			
+
 			$atts = (array) $rah_swap[$atts['link']];
 		}
 
 		$opt = lAtts(array(
-			'link' => '',
-			'reset' => 0,
-			'db' => NULL,
-			'user' => '',
-			'pass' => '',
-			'host' => 'localhost',
-			'dbcharset' => 'utf8',
+			'link'         => '',
+			'reset'        => 0,
+			'db'           => null,
+			'user'         => '',
+			'pass'         => '',
+			'host'         => 'localhost',
+			'dbcharset'    => 'utf8',
 			'client_flags' => 0,
 		), $atts);
-		
+
 		extract($opt);
-		
-		if(count($atts) == 1 && $db !== NULL) {
+
+		if (count($atts) == 1 && $db !== null)
+		{
 			mysql_select_db($db, $GLOBALS['DB']->link);
 		}
-		
-		else if(!$reset) {
+		else if (!$reset)
+		{
 			mysql_close($GLOBALS['DB']->link);
 			$txpcfg = $opt;
 			$GLOBALS['DB'] = new DB;
 		}
-		
+
 		$r = $thing !== NULL && ($reset = 1) ? parse($thing) : '';
-		
-		if($reset) {
+
+		if ($reset)
+		{
 			mysql_close($GLOBALS['DB']->link);
 			$txpcfg = $default_cfg;
 			$GLOBALS['DB'] = new DB;
 		}
-		
+
 		return $r;
 	}
 ?>
